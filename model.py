9acc98a06a71e854a516470e4b9cbcd7875bca75
Akshay Kakkar <aksh61094@gmail.com>
2018-02-27 23:59:39 +0530

    commit one
    beahvioral cloning

---
+++ model.py
@@ -0,0 +1,62 @@
+import csv
+import cv2
+from sklearn.utils import shuffle
+import sklearn
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Lambda, Convolution2D, Dropout, Cropping2D
+from keras.layers.pooling import MaxPooling2D
+import random
+
+
+lines = []
+index = 0
+with open('data/driving_log.csv') as csvFile:
+    reader = csv.reader(csvFile)
+    for i in reader:
+        if index !=0:
+            lines.append(i)
+        index =1
+images = []
+measures = []
+for line in lines:
+    select = random.choice([0,1,2])
+    src = line[select]
+    filename = src.split('/')[-1]
+    curr_path = 'data/IMG/' + filename
+    image = cv2.imread(curr_path)
+    if select == 1:
+        angle = float(line[3]) +0.25
+    elif select == 2:
+        angle = float(line[3]) -0.25
+    else:
+        angle = float(line[3])
+    images.append(image)
+    measures.append(angle)
+    if random.random() > 0.5:
+        images.append(cv2.flip(image,1))
+        measures.append(angle*-1.0)
+    
+X_train = np.array(images)
+y_train = np.array(measures)
+
+print(X_train.shape,y_train.shape)
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(24, 5, 5, activation='relu', subsample=(2, 2)))
+model.add(Convolution2D(36, 5, 5, activation='relu', subsample=(2, 2)))
+model.add(Convolution2D(48, 5, 5, activation='relu', subsample=(2, 2)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(1))
+model.summary()
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, nb_epoch=3, validation_split=0.2, shuffle=True, batch_size=32)
+model.save('behav.h5')
